f50f0a89acf20d7cffa9581cf0b5aacb24105f1c
23945 <2394555023@qq.com>
2022-04-22 23:12:18 +0800

    0

--- WinhttpAPI.cpp
+++ WinhttpAPI.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <vector>
 #include "Windows.h"
+#include "winhttp.h"
+#pragma comment(lib,"winhttp.lib")
 
+using namespace std;
 
-void c_start(){
+struct WinhttpSetting{
 
+}stWinhttpSettings;
 
-
+bool initWinhttp(){
+    WinHttpOpen()
+}
+char *Winhttp_Get(char *szUrl,vector<char *>szHandles,char *szCookies,char *szProxy,unsigned uTimeout){
 
 }
+char *Winhttp_Post(){
 
+}
 
--- WinhttpAPI.h
+++ WinhttpAPI.h
@@ -5,8 +5,8 @@
 #ifndef WINHTTP_SIMPLEAPI_WINHTTPAPI_H
 #define WINHTTP_SIMPLEAPI_WINHTTPAPI_H
 
-char* Winhttp_Get();
-char* Winhttp_Post();
+char *Winhttp_Get();
+char *Winhttp_Post();
 
 
 #endif //WINHTTP_SIMPLEAPI_WINHTTPAPI_H
